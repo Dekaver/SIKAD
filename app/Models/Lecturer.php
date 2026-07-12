21ae85384e238497f168844a27a52bb25c3a6f3b
Dekaver <alwiwahyudi8@gmail.com>
2026-07-13 07:46:41 +0800

    Add Lecturer model with fillable attributes and relationships to Major and User

--- app/Models/Lecturer.php
+++ app/Models/Lecturer.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lecturer extends Model
 {
-    //
+    protected $table = 'lecturers';
+
+    protected $fillable = [
+        'user_id',
+        'major_id',
+        'nidn',
+        'name',
+        'place_of_birth',
+        'birth_date',
+        'gender',
+        'address',
+        'email',
+        'phone',
+    ];
+
+    public function major()
+    {
+        return $this->belongsTo(Major::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
