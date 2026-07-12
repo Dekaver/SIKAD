b3d8043482c898ac902b4727c070dcf72ecfbdda
Dekaver <alwiwahyudi8@gmail.com>
2026-07-13 07:46:47 +0800

    Add Student model with fillable attributes and relationships to Major and User

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    protected $table = 'students';
+
+    protected $fillable = [
+        'user_id',
+        'major_id',
+        'nim',
+        'email',
+        'name',
+        'phone',
+        'place_of_birth',
+        'birth_date',
+        'gender',
+        'address',
+        'academic_year',
+        'term',
+    ];
+
+    public function major()
+    {
+        return $this->belongsTo(Major::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
